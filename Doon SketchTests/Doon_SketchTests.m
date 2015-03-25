7d037e6608892f3b72519d00c098645b847b5aee
Guangjian Zhang <zhangguangjian@live.cn>
2015-03-25 14:20:03 +0800

    Added some other files

---
+++ Doon SketchTests/Doon_SketchTests.m
@@ -0,0 +1,40 @@
+//
+//  Doon_SketchTests.m
+//  Doon SketchTests
+//
+//  Created by 张 光建 on 15/3/25.
+//  Copyright (c) 2015年 Doonsoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface Doon_SketchTests : XCTestCase
+
+@end
+
+@implementation Doon_SketchTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
